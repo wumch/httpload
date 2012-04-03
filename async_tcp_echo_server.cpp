e2b338180fa59930f9809cc2487fca8c1c90474b
wumengchun <wumengchun@gonline.com>
2012-04-03 09:51:53 +0800

    pefect implements

---
+++ async_tcp_echo_server.cpp
@@ -0,0 +1,168 @@
+//
+// async_tcp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include "extra_header_resolver.hpp"
+
+using boost::asio::ip::tcp;
+
+class session
+{
+public:
+	session(boost::asio::io_service& io_service)
+		: socket_(io_service), timer(io_service)
+	{
+	}
+
+	tcp::socket& socket()
+	{
+		return socket_;
+	}
+
+	void start()
+	{
+		//* usage 1:
+		gonline::tgw::resolve_extra_header(
+			socket_, data_,
+		    boost::bind(&session::handle_read, this,
+		        boost::asio::placeholders::error,
+		        boost::asio::placeholders::bytes_transferred),
+			boost::bind(&session::on_extrea_header_error, this,
+				boost::asio::placeholders::error)
+		);
+		// end usage 1 */
+
+		/* usage 2: (exits extra-header-resolver after timerout).
+		timer.expires_from_now(boost::posix_time::millisec(3000));
+
+		gonline::tgw::resolve_extra_header(
+			socket_, data_,
+			boost::bind(&session::handle_read, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred),
+			boost::bind(&session::on_extrea_header_error, this,
+				boost::asio::placeholders::error),
+			timer
+		);
+		// end usage 2 */
+	}
+
+	void on_extrea_header_error(const boost::system::error_code& error)
+	{
+		socket_.cancel();
+		delete this;
+	}
+
+	void handle_read(const boost::system::error_code& error,
+	    size_t bytes_transferred)
+	{
+		if (!error)
+		{
+			boost::asio::async_write(
+			    socket_,
+			    boost::asio::buffer(data_, bytes_transferred),
+			    boost::bind(&session::handle_write, this,
+			        boost::asio::placeholders::error));
+		}
+		else
+		{
+			delete this;
+		}
+	}
+
+	void handle_write(const boost::system::error_code& error)
+	{
+		if (!error)
+		{
+			socket_.async_read_some(
+			    boost::asio::buffer(data_, max_length),
+			    boost::bind(&session::handle_read, this,
+			        boost::asio::placeholders::error,
+			        boost::asio::placeholders::bytes_transferred));
+		}
+		else
+		{
+			delete this;
+		}
+	}
+
+//private:
+	tcp::socket socket_;
+	boost::asio::deadline_timer timer;
+	static const std::size_t max_length = 1024;
+	char data_[max_length];
+};
+
+class server
+{
+public:
+	server(boost::asio::io_service& io_service, short port)
+		: io_service_(io_service), acceptor_(io_service,
+		    tcp::endpoint(tcp::v4(), port))
+	{
+		session* new_session = new session(io_service_);
+		acceptor_.async_accept(
+		    new_session->socket(),
+		    boost::bind(&server::handle_accept, this, new_session,
+		        boost::asio::placeholders::error));
+	}
+
+	void handle_accept(session* new_session,
+	    const boost::system::error_code& error)
+	{
+		if (!error)
+		{
+			new_session->start();
+			new_session = new session(io_service_);
+			acceptor_.async_accept(
+			    new_session->socket(),
+			    boost::bind(&server::handle_accept, this, new_session,
+			        boost::asio::placeholders::error));
+		}
+		else
+		{
+			delete new_session;
+		}
+	}
+
+private:
+	boost::asio::io_service& io_service_;
+	tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: async_tcp_echo_server <port>\n";
+			return 1;
+		}
+
+		boost::asio::io_service io_service;
+
+		using namespace std;
+		// For atoi.
+		server s(io_service, atoi(argv[1]));
+
+		io_service.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
--- extra_header_resolver.hpp
+++ extra_header_resolver.hpp
@@ -1,84 +1,175 @@
+/**
+ * <class gonline::tgw::ExtraHeaderResolver>
+ * implements Tencent's TGW(Tencent Gateway) protocol.
+ *
+ * include (header only, requires boost):
+ * #include "extra_header_resolver.hpp"
+ *
+ * compile (requires -lboost_system -pthread, DEBUG=0,1,>=2 supported):
+ * g++ -DDEBUG=2 -Wall -g3 -lboost_system -pthread your_program.cpp -o your_program
+ *
+Semantics (strongly exception-safe, <just IMO ^_^>):
+
+void resolve_extra_header(
+	// socket, supports <boost::asio::ip::tcp::socket> obly.
+	boost::asio::ip::tcp::socket& socket,
+
+	// socket receive buffer. eg. <char[1024]>.
+	AnyType buffer[buffer_capacity],
+
+	// success callback, must be `copy-construct-able`.
+	// all of <func-pointer>, <boost::bind(...)>, <member-pointer> are ok.
+	// NOTE: pass in as:    <const SuccessCallback&>.
+	void (SuccessCallback*) (boost::system::error_code& error),
+
+	// error callback, must be `copy-construct-able`.
+	// all of <func-pointer>, <boost::bind(...)>, <member-pointer> are ok.
+	// NOTE: pass in as:    <const ErrorCallback&>.
+	void (ErrorCallback*) (boost::system::error_code& error, const std::size_t bytes_buffered),
+
+	// optional timer, indicates timeout-waiter.
+	// once wait timeout, we will do:
+	// 	   socket.cancel();	// NOTE: we will NOT disconnect.
+	// 	   delete <ExtraHeaderResolver>;
+	boost::asio::deadline_timer& timer
+);
+
+// *********************** call usage 1: ***********************
+gonline::tgw::resolve_extra_header(
+	socket_, data_,
+	boost::bind(&session::handle_read, this,
+		boost::asio::placeholders::error,
+		boost::asio::placeholders::bytes_transferred),
+	boost::bind(&session::on_extrea_header_error, this,
+		boost::asio::placeholders::error)
+);
+// *********************** end call usage 1 *********************** /
+
+// ********** call usage 2: (exits extra-header-resolver after timerout).*************
+boost::asio::deadline_timer timer(<boost::asio::io_service>);
+timer.expires_from_now(boost::posix_time::millisec(3000));
+gonline::tgw::resolve_extra_header(
+	socket_, data_,
+	boost::bind(&session::handle_read, this,
+		boost::asio::placeholders::error,
+		boost::asio::placeholders::bytes_transferred),
+	boost::bind(&session::on_extrea_header_error, this,
+		boost::asio::placeholders::error),
+	timer
+);
+// *********************** end call usage 2 *********************** /
+ *
+ * the `extra_header` concept in TGW:
+ * 	   extra_header is a http-protocol-like ASCII header which required by Tencent's TGW-server.
+ * 	   It must be sent by game-client once connection is built up.
+ * 	   Content of extra_header should looks like "GET / HTTP/1.1\r\nHost: app26745-1.qzoneapp.com:8000\r\n\r\n".
+ * 	   Usually, the content of extra_header is runtime-constant for each server-process.
+ *
+ * The purpose of <gonline::tgw::ExtraHeaderResolver> is to
+ * ``implements TGW-protocol by only adds one-line-of-code (or fews more), and zero-line-of-code need modification``.
+ * So, <gonline::tgw::ExtraHeaderResolver> will receives, authorizes, and then removes extra_header
+ * from received data, then provides you with actually available data.
+ *
+ * The work follow of <gonline::tgw::ExtraHeaderResolver>:
+ * start() => receive_header() => auth_header() => SuccessCallback.
+ */
 
 #pragma once
 
 #include <cstddef>
 #include <cstdlib>
 #include <string>
-#include <iostream>
 #include <boost/cstdint.hpp>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#ifndef GOL_DEBUG
+#	if defined(DEBUG) && !defined(NDEBUG)
+#		define GOL_DEBUG	DEBUG
+#	else
+#		define GOL_DEBUG	0
+#	endif
+#endif
 
 // flag indicates whether the content of `extra_header` is constant in run-time or not.
 #ifndef GOL_EXTRA_HEADER_CONST
 #	define GOL_EXTRA_HEADER_CONST 0
-#else
-#	error "macro name GOL_EXTRA_HEADER_CONST is taken."
 #endif
 
-#if defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST
-#	include <sstream>
-#else
+#if !(defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST)
 #	include <boost/algorithm/string/find.hpp>
 #	include <boost/static_assert.hpp>
-#	define EXTRA_HEADER_MAX_LENGTH 500
+#	ifndef GOL_EXTRA_HEADER_MAX_LENGTH
+//#		define GOL_EXTRA_HEADER_MAX_LENGTH ULONG_LONG_MAX
+#		define GOL_EXTRA_HEADER_MAX_LENGTH 200
+#	endif
 #endif
 
-#ifndef GOL_STRLEN
-#	define GOL_STRLEN(s) (sizeof(s) / sizeof(char) - 1)
+#if defined(OC_BLACK) || defined(GOL_OC_RED) || defined(GOL_OC_GREEN) ||	\
+	defined(GOL_OUT) || defined(GOL_SAY) || defined(GOL_ERR) ||				\
+	defined(GOL_MIN) ||								\
+	defined(GOL_STRLEN) ||							\
+	defined(GOL_EXTRA_HEADER_TAIL) ||				\
+	defined(GOL_EXTRA_HEADER_MIN_LENGTH) ||			\
+	defined(GOL_LIKELY) || defined(GOL_UNLIKELY)
+#	error "macro name GOL_xxx... is taken."
+#endif
+
+#define GOL_OC_BLUE(val)	"\033[32;34;5m" << val << "\033[0m"
+#define GOL_OC_RED(val)		"\033[32;31;5m" << val << "\033[0m"
+#define GOL_OC_GREEN(val)	"\033[32;49;5m" << val << "\033[0m"
+#define GOL_STRLEN(s) 	(sizeof(s) / sizeof(char) - 1)
+#define GOL_MIN(a,b) 	((a) <= (b) ? (a) : (b))
+#define GOL_EXTRA_HEADER_TAIL		"\r\n\r\n"
+#define GOL_EXTRA_HEADER_MIN_LENGTH (GOL_STRLEN("GET / HTTP/1.1\r\nHost:x.xx") + GOL_STRLEN(GOL_EXTRA_HEADER_TAIL))
+
+#if GOL_DEBUG > 1
+#define GOL_OUT(ostream, msg) \
+	std::cout <<  GOL_OC_BLUE(__FILE__) << ":" << GOL_OC_BLUE(__LINE__) \
+	<< ":\t" << msg << std::endl;
+#elif GOL_DEBUG
+#	define GOL_OUT(ostream, msg)	ostream << msg << std::endl;
 #else
-#	error "macro name GOL_STRLEN is taken."
+#	define GOL_OUT(ostream, msg)
+#endif
+
+#define GOL_SAY(msg)	GOL_OUT(std::cout, msg)
+#define GOL_ERR(msg) 	GOL_OUT(std::cerr, GOL_OC_RED(msg))
+#define GOL_DUMP(var)	GOL_OUT(std::cout, GOL_OC_BLUE(#var) << ": " << GOL_OC_GREEN(var))
+
+#ifdef __GNUC__
+#	define GOL_LIKELY(expr)		__builtin_expect((expr), 1)
+#	define GOL_UNLIKELY(expr)	__builtin_expect((expr), 0)
+#else
+#	define GOL_LIKELY(expr) 	(expr)
+#	define GOL_UNLIKELY(expr)	(expr)
 #endif
 
 namespace gonline {
 namespace tgw {
 
-template<typename SuccessCallback, typename ErrorCallback>
+namespace bap = ::boost::asio::placeholders;
+typedef boost::asio::ip::tcp::socket Sock;
+typedef uint16_t	port_t;
+typedef std::size_t	buf_size_t;
+typedef ssize_t		buf_ssize_t;
+typedef char		byte_t;		// typeof(`element of socket stream`)
+
+template<
+	typename Buffer, buf_size_t _buffer_capacity,
+	typename SuccessCallback, typename ErrorCallback
+>
 class ExtraHeaderResolver
+	: public boost::enable_shared_from_this<ExtraHeaderResolver<Buffer, _buffer_capacity, SuccessCallback, ErrorCallback> >
 {
-	namespace bap = ::boost::asio::placeholders;
-protected:
-	typedef boost::asio::ip::tcp::socket Sock;
-	typedef uint16_t	port_t;
-	typedef std::size_t	buf_size_t;
-	typedef ssize_t		buf_ssize_t;
-	typedef char		byte_t;		// typeof(`element of socket stream`)
-
-	Sock& sock;
-	SuccessCallback&	success_callback;
-	ErrorCallback&		error_callback;
-
-	byte_t* buffer;
-	static const std::size_t buffer_cap;
-	buf_size_t bytes_buffered;	// count of elements which are already inside <data_>.
-
-#if defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST
-	static std::string extra_header;	// content of extra_header.
-#else
-	static const buf_size_t extra_header_lpos = GOL_STRLEN("GET / HTTP/1.1\r\nHost:x.xx");
-	static const buf_size_t extra_header_rpos = extra_header_lpos + 500;
-	BOOST_STATIC_ASSERT(extra_header_rpos <= buffer_cap - GOL_STRLEN("\r\n\r\n"));
-	BOOST_STATIC_ASSERT(extra_header_lpos <= extra_header_rpos);
-#endif
-
 public:
-	template<typename Buffer, buf_size_t _buffer_capacity>
-	ExtraHeaderResolver(Sock& _sock, Buffer& _buffer,
-		SuccessCallback _success_callback, ErrorCallback& _error_callback)
-		: sock(_sock), buffer(_buffer), buffer_cap(_buffer_capacity),
+	ExtraHeaderResolver(Sock& _sock, Buffer (&_buffer)[_buffer_capacity],
+		const SuccessCallback& _success_callback, const ErrorCallback& _error_callback)
+		: sock(_sock), buffer(reinterpret_cast<byte_t*>(_buffer)), bytes_buffered(0),
 		  success_callback(_success_callback), error_callback(_error_callback)
 	{
-
-	}
-
-	template<typename Buffer, buf_size_t _buffer_capacity>
-	ExtraHeaderResolver(Sock& _sock, Buffer& _buffer[_buffer_capacity],
-		SuccessCallback _success_callback, ErrorCallback& _error_callback)
-		: sock(_sock), buffer(_buffer), buffer_cap(_buffer_capacity),
-		  success_callback(_success_callback), error_callback(_error_callback)
-	{
-
 	}
 
 	void start()
@@ -86,11 +177,20 @@ public:
 		receive_header();
 	}
 
-	// test only dtor.
-	~session()
+	// stop by timeout
+	void stop()
 	{
-		std::cout << "disconnecting..." << std::endl;
+		sock.cancel();
+//		delete this;
 	}
+
+#if GOL_DEBUG
+	// test only dtor.
+	~ExtraHeaderResolver()
+	{
+		GOL_SAY("exiting extra-header-resolver...")
+	}
+#endif
 
 protected:
 	/**
@@ -100,8 +200,8 @@ protected:
 	void receive_header()
 	{
 		sock.async_read_some(
-			boost::asio::buffer(buffer + bytes_buffered, buffer_cap - bytes_buffered),
-			boost::bind(&ExtraHeaderResolver::auth_header, this, bap::error, bap::bytes_transferred)
+			boost::asio::buffer(buffer + bytes_buffered, buffer_capacity - bytes_buffered),
+			boost::bind(&ExtraHeaderResolver::auth_header, this->shared_from_this(), bap::error, bap::bytes_transferred)
 		);
 	}
 
@@ -111,58 +211,69 @@ protected:
 	 */
 	void auth_header(const boost::system::error_code& error, const buf_size_t bytes_transferred)
 	{
-		if (!error)
+		if (GOL_LIKELY(!error))
 		{
 			bytes_buffered += bytes_transferred;
 #if defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST
-			if (bytes_buffered >= extra_header.size())	// extra_header is complete
+			if (GOL_LIKELY(bytes_buffered >= extra_header.size()))	// extra_header is complete
 #else
-			if (bytes_buffered >= extra_header_lpos)
+			GOL_DUMP(bytes_buffered)
+			GOL_DUMP(extra_header_lpos)
+			if (GOL_LIKELY(bytes_buffered >= extra_header_lpos))
 #endif
 			{
 				buf_ssize_t extra_header_len;
-				if ((extra_header_len = resolve_extra_header()) >= 0)	// extra_header is correct.
+				if (GOL_LIKELY((extra_header_len = resolve_extra_header()) >= 0))	// extra_header is correct.
 				{
 					// we'll remove extra_header from `data_`,
 					// so that you can use this `data_` as you used to do.
-					if (bytes_buffered == static_cast<buf_size_t>(extra_header_len))		// received bytes are exactly the `extra_header`.
+					GOL_DUMP(bytes_buffered)
+					GOL_DUMP(extra_header_len)
+					if (GOL_LIKELY(bytes_buffered == static_cast<buf_size_t>(extra_header_len)))		// received bytes are exactly the `extra_header`.
 					{
-						success_callback(bytes_buffered = 0);
+						success_callback(error, bytes_buffered = 0);
+						GOL_SAY(GOL_OC_GREEN("extra-header is exactly matched."))
 					}
 					else	// some additional bytes behind `extra_header`.
 					{
 						bytes_buffered -= extra_header_len;
-						if (bytes_buffered <= static_cast<buf_size_t>(extra_header_len))
+						if (GOL_LIKELY(bytes_buffered <= static_cast<buf_size_t>(extra_header_len)))
 						{
 							std::memcpy(buffer, buffer + extra_header_len, bytes_buffered);
 						}
 						else
 						{
 							// these memcpy() calls can be optimized out if your `buffer_` has `offset` supported:
-							byte_t tmp_data[buffer_cap];
+							byte_t tmp_data[buffer_capacity];
 							std::memcpy(tmp_data, buffer + extra_header_len, bytes_buffered);
 							std::memcpy(buffer, tmp_data, bytes_buffered);
 						}
-						std::cout << "header is correct, " << bytes_buffered << " bytes remains in buffer..." << std::endl;
-						success_callback(bytes_buffered);	// handle additional bytes.
+						GOL_SAY(GOL_OC_GREEN("extra-header is correct, ") << GOL_OC_RED(bytes_buffered) << GOL_OC_GREEN(" bytes remains in buffer..."))
+						success_callback(error, (bytes_buffered));	// handle additional bytes.
 					}
 				}
+#if !(defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST)
+				else if (bytes_buffered < extra_header_rpos)
+				{
+					GOL_ERR("received incomplete extra-header, cumulative length: " << bytes_buffered << ", continue with receive.")
+					receive_header();
+				}
+#endif
 				else	// received extra_header is wrong, just disconnect.
 				{
-					std::cerr << "received wrong extra_header, disconnecting." << std::endl;
+					GOL_ERR("received wrong extra-header, disconnecting.")
 					error_callback(error);
 				}
 			}
 			else	// extra_header is incomplete, continue with receive_header().
 			{
-				std::cerr << "received incomplete header, cumulative length: "
-					<< bytes_buffered << ", continue to receive." << std::endl;
+				GOL_ERR("extra-header is incomplete, cumulative length: " << bytes_buffered << ", continue with receive.")
 				receive_header();
 			}
 		}
 		else	// socket error
 		{
-			std::cerr << "socket error, disconnecting." << std::endl;
+			GOL_ERR("socket error: " << error)
 			error_callback(error);
 		}
 	}
@@ -171,34 +282,25 @@ protected:
 	 * validate `received extra_header`
 	 * @return `received extra_header`.length, if it's correct, else -1.
 	 */
-	__attribute__((always_inline)) buf_ssize_t resolve_extra_header() const
+	__attribute__((always_inline)) buf_ssize_t resolve_extra_header()
 	{
 #if defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST
 		return extra_header.compare(0, extra_header.size(),
-			buffer_, extra_header.size()) == 0 ? extra_header.size() : -1;
+			buffer, extra_header.size()) == 0 ? extra_header.size() : -1;
 #else
 		// in this case, we just find the first position of "\r\n\r\n" in `buffer_`:
 		// length of the shortest `extra_header`
 //* The first implemention:
 		return recheck(
 			boost::find_first<const boost::iterator_range<const byte_t*>, const char*>(
-				boost::make_iterator_range(buffer + extra_header_lpos, buffer + std::min(bytes_buffered, extra_header_rpos)),
-				"\r\n\r\n"
+				boost::make_iterator_range(buffer + extra_header_lpos, buffer + GOL_MIN(bytes_buffered, extra_header_rpos)),
+				GOL_EXTRA_HEADER_TAIL
 			).begin() - buffer
 		);
 // end The first implemention */
 
 /* The second implemention:
-		// this is another implemention which not requires boost.
-#	ifndef GOL_UNLIKELY
-#		ifdef __GNUC__
-#			define GOL_LIKELY(expr)		__builtin_expect((expr), 1)
-#			define GOL_UNLIKELY(expr)	__builtin_expect((expr), 0)
-#		else
-#			define GOL_LIKELY(expr) 	(expr)
-#			define GOL_UNLIKELY(expr)	(expr)
-#		endif
-#	endif
+		// this is another implemention which does not require boost.
 		for (buf_size_t i = extra_header_lpos, end = std::min(bytes_buffered, extra_header_rpos);
 			i < end; ++i)
 		{
@@ -219,24 +321,88 @@ protected:
 	static void reset_extra_header(const std::string& domain, const port_t port)
 	{
 		extra_header.clear();
-		// string(port).size() <= 5
-		extra_header.reserve((GOL_STRLEN("GET / HTTP/1.1\r\nHost: " ":" "\r\n\r\n") + 5) + domain.size());
+		extra_header.reserve(GOL_STRLEN("GET / HTTP/1.1\r\nHost: " ":" GOL_EXTRA_HEADER_TAIL "65535") + domain.size());
 		extra_header.append("GET / HTTP/1.1\r\nHost: ");
 		extra_header.append(domain);
 		extra_header.append(":");
 		extra_header.append(boost::lexical_cast<std::string>(port));
-		extra_header.append("\r\n\r\n");
+		extra_header.append(GOL_EXTRA_HEADER_TAIL);
 	}
 #endif
 
 #if !(defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST)
-	// to make auth_header() contains no temporary-variables, so that auth_header() can be inlined.
+	// assist `tpl-expr` to make auth_header() contains no temporary-variables, so that auth_header() can be inlined.
 	__attribute__((always_inline)) buf_ssize_t recheck(const buf_size_t pos) const
 	{
-		return pos > buffer_cap ? -1 : (pos + GOL_STRLEN("\r\n\r\n"));
+		return pos < bytes_buffered ? (pos + GOL_STRLEN(GOL_EXTRA_HEADER_TAIL)) : -1;
 	}
 #endif
+
+protected:
+	Sock& sock;
+
+	byte_t* buffer;
+	static const std::size_t buffer_capacity = _buffer_capacity;
+	BOOST_STATIC_ASSERT(buffer_capacity >= GOL_EXTRA_HEADER_MIN_LENGTH);
+	BOOST_STATIC_ASSERT(buffer_capacity > GOL_STRLEN(GOL_EXTRA_HEADER_TAIL));
+	buf_size_t bytes_buffered;	// count of elements which are already inside <data_>.
+
+	SuccessCallback	success_callback;
+	ErrorCallback		error_callback;
+
+#if defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST
+	static std::string extra_header;	// content of extra_header.
+#else
+	static const std::size_t extra_header_lpos = GOL_EXTRA_HEADER_MIN_LENGTH;
+	static const std::size_t extra_header_rpos = GOL_MIN(GOL_EXTRA_HEADER_MAX_LENGTH, buffer_capacity - GOL_STRLEN(GOL_EXTRA_HEADER_TAIL));
+	BOOST_STATIC_ASSERT(extra_header_rpos <= buffer_capacity - GOL_STRLEN(GOL_EXTRA_HEADER_TAIL));
+	BOOST_STATIC_ASSERT(extra_header_lpos <= extra_header_rpos);
+#endif
 };	// end class ExtraHeaderResolver
+
+#if defined(GOL_EXTRA_HEADER_CONST) && GOL_EXTRA_HEADER_CONST
+template<typename Buffer, buf_size_t _buffer_capacity,
+	typename SuccessCallback, typename ErrorCallback>
+std::string ExtraHeaderResolver<Buffer, _buffer_capacity,
+	SuccessCallback, ErrorCallback>::extra_header;
+#endif
+
+template<typename Buffer, buf_size_t buffer_capacity,
+	typename SuccessCallback, typename ErrorCallback>
+inline ExtraHeaderResolver<Buffer, buffer_capacity,
+	SuccessCallback, ErrorCallback>*
+make_resolver(
+	Sock& sock, Buffer (&buffer)[buffer_capacity],
+	const SuccessCallback& scb, const ErrorCallback& ecb)
+{
+	return new ExtraHeaderResolver<Buffer, buffer_capacity,
+		SuccessCallback, ErrorCallback>(sock, buffer, scb, ecb);
+}
+
+template<typename Buffer, buf_size_t buffer_capacity,
+	typename SuccessCallback, typename ErrorCallback>
+inline void resolve_extra_header(
+	Sock& sock, Buffer (&buffer)[buffer_capacity],
+	const SuccessCallback& scb, const ErrorCallback& ecb)
+{
+	typedef ExtraHeaderResolver<Buffer, buffer_capacity, SuccessCallback, ErrorCallback> EHR;
+	boost::shared_ptr<EHR> ehr(make_resolver(sock, buffer, scb, ecb));
+	ehr->start();
+}
+
+template<typename Buffer, buf_size_t buffer_capacity,
+	typename SuccessCallback, typename ErrorCallback>
+inline void resolve_extra_header(
+	Sock& sock, Buffer (&buffer)[buffer_capacity],
+	const SuccessCallback& scb, const ErrorCallback& ecb,
+	boost::asio::deadline_timer& timer)
+{
+	typedef ExtraHeaderResolver<Buffer, buffer_capacity, SuccessCallback, ErrorCallback> EHR;
+	boost::shared_ptr<EHR> ehr(make_resolver(sock, buffer, scb, ecb));
+	timer.async_wait(boost::bind(&EHR::stop, ehr));
+	ehr->start();
+}
+
 
 }	// end namespace tgw
 }	// end namespace gonline
